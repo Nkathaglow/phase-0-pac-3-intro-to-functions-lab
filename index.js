521738ded67dec40901620f8aefc37874f4b880d
Nkatha_Glory <kinyua6945@gmail.com>
2023-03-07 11:03:51 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout (Hello) {
+    console.log(Hello.toUpperCase( ));
+}
+
+function logWhisper (Hello) {
+    console.log(Hello.toLowerCase( ));
+}
+
+function sayHiToHeadphonedRoommate (HELLO) 
+ if(HELLO.toLowerCase) {
+    console.log("I can\'t hear you!")
+ }else if(HELLO.toUpperCase) {
+    console.log("YES INDEED!")
+ }else( "Let\'s have diiner together!") {
+    console.log("I would love to!")
+ }
+    
+
+ 
+    
